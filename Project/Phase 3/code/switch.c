3281cf223af4eaa7c772bf84b218137dcd42d442
SYED WALI UDDIN <61510401+uddinwali@users.noreply.github.com>
2022-08-15 20:35:22 +0500

    Add files via upload

---
+++ Project/Phase 3/code/switch.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+    int inp;
+
+    inp = 2;
+
+    switch ( inp )
+    {
+         case 1 : 
+            printf( "\Ali" );
+            break;
+         case 2 : 
+            printf( "\Azhar" );
+            break;
+         case 3 : 
+            printf( "\Usman");
+            break;
+         case 4 : 
+            printf( "\Wali" );
+            break;
+         case 5 : 
+            printf( "\naufil" );
+            break;
+         
+        default : 
+            printf( "\CompilerConstruction");
+    }
+
+    return 0;
+}
