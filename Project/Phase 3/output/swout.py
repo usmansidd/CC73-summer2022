4cabad39f5af44f5a060829bf401afb38cbecaf3
SYED WALI UDDIN <61510401+uddinwali@users.noreply.github.com>
2022-08-15 20:36:38 +0500

    Add files via upload

---
+++ Project/Phase 3/output/swout.py
@@ -0,0 +1,24 @@
+def main():
+	
+	inp = 2
+	match inp:
+		case 1:
+			print("\Ali")
+			
+		case 2:
+			print("\Azhar")
+			
+		case 3:
+			print("\Usman")
+			
+		case 4:
+			print("\Wali")
+			
+		case 5:
+			print("\naufil")
+			
+		case _ :
+	 		print("\CompilerConstruction")
+		
+	return 0
+	
